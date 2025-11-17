6f3b2c38d983363348c5cf8e244997b43c4141be
NoahPeters8 <noah.peters@bobcats.gcsu.edu>
2025-11-16 21:50:59 -0500

    Create Week2PT1

---
+++ Week2PT1.py
@@ -0,0 +1,21 @@
+#Script 1
+name = input("Enter your name: ")
+print("You entered:", name)
+print("Data type:", type(name))
+
+##Script 2
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+total = num1 + num2
+
+print("The total is:", total)
+print("Data type of total:", type(total))
+
+#Script 3
+celsius = float(input("Enter a temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+print("Data type:", type(fahrenheit))
